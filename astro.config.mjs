f78ad778e990189971dec9cd21d84c3b21a816ec
Gabriel Matte Elias <gabrielmatteelias@gmail.com>
2026-06-08 16:56:42 -0300

    implementado o sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://trends-eta-three.vercel.app',
+  integrations: [tailwind(), sitemap()],
   vite: {
     ssr: {
       external: ['svgo']
